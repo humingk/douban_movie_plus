3fc3f3748e5f68fa11f1a841188f6e133b25c86b
humingk <humingk@qq.com>
2019-12-25 19:31:17 +0800

    add bus,need to fix pom

--- movie-config-client/src/main/java/org/humingk/config/controller/ConfigClientController.java
+++ movie-config-client/src/main/java/org/humingk/config/controller/ConfigClientController.java
@@ -1,6 +1,7 @@
 package org.humingk.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @author humingk
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
     /**
-     * 来自server配置属性
+     * 来自config server从git获取的配置属性
      */
     @Value("${profile}")
     private String profile;
--- movie-config-server/src/main/java/org/humingk/config/MovieConfigServerApplication.java
+++ movie-config-server/src/main/java/org/humingk/config/MovieConfigServerApplication.java
@@ -2,13 +2,19 @@ package org.humingk.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author humingk
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableDiscoveryClient
+@EnableEurekaClient
+@RefreshScope
 public class MovieConfigServerApplication {
 
     public static void main(String[] args) {
