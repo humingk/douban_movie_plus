4e239f34f7a10ae9c6575b5ba756313822537157
mingjun.hu <mingjun.hu@qunar.com>
2021-01-28 21:38:39 +0800

    fix(doubans): 恢复资源模块

--- movie-api/src/main/java/org/humingk/movie/api/movie/ResourceApi.java
+++ movie-api/src/main/java/org/humingk/movie/api/movie/ResourceApi.java
@@ -1,7 +1,16 @@
 package org.humingk.movie.api.movie;
 
+import org.humingk.movie.api.common.vo.ResourceMovieVo;
+import org.humingk.movie.common.entity.Result;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
 
 /**
  * 电影资源
@@ -11,18 +20,18 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 @FeignClient("movie-server-movie")
 public interface ResourceApi {
-  //  /**
-  //   * 电影资源
-  //   *
-  //   * @param id 豆瓣电影ID
-  //   * @param limit 限制数（默认30）
-  //   * @return
-  //   * @since 2.0.0
-  //   * @apiNote 包括正版资源、盗版资源、在线资源、离线资源
-  //   */
-  //  @RequestMapping(value = "/movie/resource/bases", method = RequestMethod.GET)
-  //  Result<List<ResourceMovieVo>> bases(
-  //      @RequestParam("id") @NotNull Long id,
-  //      @RequestParam(value = "limit", required = false, defaultValue = "30") @PositiveOrZero
-  //          Integer limit);
+      /**
+       * 电影资源
+       *
+       * @param id 豆瓣电影ID
+       * @param limit 限制数（默认30）
+       * @return
+       * @since 2.0.0
+       * @apiNote 包括正版资源、盗版资源、在线资源、离线资源
+       */
+      @RequestMapping(value = "/movie/resource/bases", method = RequestMethod.GET)
+      Result<List<ResourceMovieVo>> bases(
+          @RequestParam("id") @NotNull Long id,
+          @RequestParam(value = "limit", required = false, defaultValue = "30") @PositiveOrZero
+              Integer limit);
 }
--- movie-servers/movie-server-movie/src/main/java/org/humingk/movie/server/movie/controller/ResourceController.java
+++ movie-servers/movie-server-movie/src/main/java/org/humingk/movie/server/movie/controller/ResourceController.java
@@ -1,21 +1,32 @@
 package org.humingk.movie.server.movie.controller;
 
+import org.humingk.movie.api.common.converter.ResourceMovieVoConverter;
+import org.humingk.movie.api.common.vo.ResourceMovieVo;
 import org.humingk.movie.api.movie.ResourceApi;
+import org.humingk.movie.common.entity.Result;
+import org.humingk.movie.service.resource.service.ResourceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
 
 /** @author humingk */
 @RestController
 public class ResourceController implements ResourceApi {
-  //  @Autowired private ResourceMovieVoConverter resourceMovieVoConverter;
-  //  @Autowired private ResourceService resourceService;
-  //
-  //  @Override
-  //  public Result<List<ResourceMovieVo>> bases(
-  //      @RequestParam("id") @NotNull Long id,
-  //      @RequestParam(value = "limit", required = false, defaultValue = "30") @PositiveOrZero
-  //          Integer limit) {
-  //    return Result.success(
-  //        resourceMovieVoConverter.toList(resourceService.getResourceListByMovieDoubanId(id,
-  // limit)));
-  //  }
+      @Autowired
+      private ResourceMovieVoConverter resourceMovieVoConverter;
+      @Autowired private ResourceService resourceService;
+
+      @Override
+      public Result<List<ResourceMovieVo>> bases(
+          @RequestParam("id") @NotNull Long id,
+          @RequestParam(value = "limit", required = false, defaultValue = "30") @PositiveOrZero
+              Integer limit) {
+        return Result.success(
+            resourceMovieVoConverter.toList(resourceService.getResourceListByMovieDoubanId(id,
+     limit)));
+      }
 }
